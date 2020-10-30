aed00200bc1b07142ea17114bae22e1b3293af90
s18529 <61682505+s18529@users.noreply.github.com>
2020-10-30 16:48:47 +0100

    zadanie 1

---
+++ Cw1/Cw1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (var httpclient = new HttpClient())
+            {
+                var response = await httpclient.GetAsync(args[0]);
+                response.EnsureSuccessStatusCode();
+                string responseToString = await response.Content.ReadAsStringAsync();
+                string[] array = responseToString.Split();
+
+                string pattern = @"([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)";
+                Regex regex = new Regex(pattern);
+                foreach (string test in array) 
+                {
+                    if (regex.IsMatch(test)) 
+                    {
+                        Console.WriteLine(test);
+                    }
+                }
+            }
+        }
+    }
+}
